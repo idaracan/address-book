562340b7abda3d18bfbf9eff4a0dba1888cdf447
Ivan Ramirez <ivandarioramirezcantillo@gmail.com>
2019-12-01 12:35:15 -0500

    added contacts route

--- index.js
+++ index.js
@@ -7,13 +7,22 @@ const app = express();
 const port = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-mongoose
-  .connect(process.env.urlDB, {
+
+// mongodb://backend:secure1@ds251158.mlab.com:51158/address-book
+mongoose.connect(
+  "mongodb://backend:secure1@ds251158.mlab.com:51158/address-book",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
-  .then(console.log("connected to database!"))
-  .catch("connection error, could not connect to database!!!");
+  },
+  (err, res) => {
+    if (err) {
+      console.log("connection error, could not connect to database!!!");
+    }
+    console.log("connected to database!");
+  }
+);
+
 app.use(require("./routes/routes"));
 
 app.listen(port, () => console.log(`listening port ${port}`));
--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,13 @@ const User = require("../models/user.js");
 const app = express();
 
 app.post("/user", (req, res) => {
-  console.log(req.body);
   const { body } = req;
   const user = new User(body.user);
-  user
-    .save()
-    .then(res.status(200).json({ message: "user saved" }))
-    .catch(err => res.status(400).json({ message: "error", err }));
+  user.save((err, newUser) => {
+    if (err) {
+      return res.status(400).json({ ...err });
+    }
+    return res.json({ message: "user saved", user: newUser });
+  });
 });
 module.exports = app;
